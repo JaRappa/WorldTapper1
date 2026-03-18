aefa00f803391f5ecc039346b95663d02a85c918
JaRappa <rappajake@gmail.com>
2026-03-18 17:36:28 -0400

    fix counter

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -83,15 +83,6 @@ export default function HomeScreen() {
   }, [isClicking]);
 
   const formatCount = (num: number): string => {
-    if (num >= 1_000_000_000) {
-      return (num / 1_000_000_000).toFixed(1) + 'B';
-    }
-    if (num >= 1_000_000) {
-      return (num / 1_000_000).toFixed(1) + 'M';
-    }
-    if (num >= 1_000) {
-      return (num / 1_000).toFixed(1) + 'K';
-    }
     return num.toLocaleString();
   };
 
